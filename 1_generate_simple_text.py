dabfe5e078112defe7347efa57b03eed11cb1d8e
Vaibhav Gupta <vg498660@gmail.com>
2025-10-24 20:15:36 +0530

    renamed:    generate_simple_text.py -> 1_generate_simple_text.py 	new file:   2_loss_calculation.py 	renamed:    preprocessing_chapter_1/1_retrieving-verdict-file.py -> preprocessing_chapter_1/_1_retrieving-verdict-file.py 	renamed:    preprocessing_chapter_1/2_SimpleTokenizerV1.py -> preprocessing_chapter_1/_2_SimpleTokenizerV1.py 	renamed:    preprocessing_chapter_1/3_SimpleTokenizerV2.py -> preprocessing_chapter_1/_3_SimpleTokenizerV2.py 	renamed:    preprocessing_chapter_1/4_BytePairEncodingTokenizer.py -> preprocessing_chapter_1/_4_BytePairEncodingTokenizer.py 	renamed:    preprocessing_chapter_1/5_slidingWindowSampling.py -> preprocessing_chapter_1/_5_slidingWindowSampling.py 	renamed:    preprocessing_chapter_1/6_CreatingDataset.py -> preprocessing_chapter_1/_6_CreatingDataset.py 	renamed:    preprocessing_chapter_1/7_EmbeddingCreation.py -> preprocessing_chapter_1/_7_EmbeddingCreation.py 	new file:   preprocessing_chapter_1/__pycache__/_6_CreatingDataset.cpython-314.pyc

--- 1_generate_simple_text.py
+++ 1_generate_simple_text.py
@@ -48,3 +48,47 @@ token_ids = generate_text_simple(
 )
 
 print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+
+file_path = "the-verdict.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text_data = file.read()
+
+total_characters = len(text_data)
+total_tokens = len(tokenizer.encode(text_data))
+print("Characters:", total_characters)
+print("Tokens:", total_tokens)
+
+train_ratio = 0.90
+split_idx = int(train_ratio * len(text_data))
+train_data = text_data[:split_idx]
+val_data = text_data[split_idx:]
+
+from preprocessing_chapter_1._6_CreatingDataset import create_dataloader
+
+train_loader = create_dataloader(
+    train_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True,
+    shuffle=True,
+    num_workers=0
+)
+val_loader = create_dataloader(
+    val_data,
+    batch_size=2,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0
+)
+
+print("Train loader:")
+for x, y in train_loader:
+    print(x.shape, y.shape)
+print("\nValidation loader:")
+for x, y in val_loader:
+    print(x.shape, y.shape)
